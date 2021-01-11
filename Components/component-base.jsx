d2d164aa57133b97a9de4e54a2355f935e1b4b83
PatoGordo <icariusv@gmail.com>
2021-01-11 19:51:26 -0300

    v1.3

--- Components/component-base.jsx
+++ Components/component-base.jsx
@@ -1,9 +1,21 @@
-class Component extends React.Component{
-    render(){
-        return(
-            <div className="Component">
-                Component
-            </div>
-        )
-    }
+class Component extends React.Component {
+  render() {
+    return (
+      <div className="Component">
+        {document.getElementsByTagName("title")[0].innerHTML = "Your Project - Title"}
+        Component
+      </div>
+    )
+  }
+}
+
+//or
+
+function Component() {
+  document.getElementsByTagName("title")[0].innerHTML = "Your Project - Title"
+  return (
+    <div className="Component">
+      Component
+    </div>
+  )
 }
