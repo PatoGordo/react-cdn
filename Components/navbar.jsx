fa28f49656d254d27d32142734bdd17a65ae1af0
patogordo <icariusv@gmail.com>
2021-01-17 22:00:34 -0300

    v2.0

--- Components/navbar.jsx
+++ Components/navbar.jsx
@@ -1,12 +1,27 @@
 class Navbar extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  handleCLick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  handleCLick2 = () => {
+    window.location.href='/'
+  }
+
   render() {
     return (
       <div className="Navbar">
-        <ul>
-          <li><Link to="/">To Home</Link></li>
-          {/*<li><Link to="/route">To Route</Link></li>*/}
+        <h1 className="navbar-logo" onClick={this.handleCLick2}>Project</h1>
+        <div className="menu-icon" onClick={this.handleCLick}>
+          <i className={ this.state.clicked ? 'fal fa-times' : 'fal fa-bars' }></i>
+        </div>
+        <ul className={ this.state.clicked ? 'nav-menu active' : 'nav-menu' } onClick={this.handleCLick}>
+          <li><Link className="nav-links" to="/">Home</Link></li>
         </ul>
-      </div>
+        </div>
     )
   }
 }
